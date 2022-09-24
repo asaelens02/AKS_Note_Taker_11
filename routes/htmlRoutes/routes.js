b309a1cd280b33e6778bac3f4e1565b9983fff14
asaelens02 <104400626+asaelens02@users.noreply.github.com>
2022-09-24 12:24:57 -0500

    add routes.js

---
+++ routes/htmlRoutes/routes.js
@@ -0,0 +1,72 @@
+//create routes
+const path = require('path');
+const router = require('express').Router();
+
+//sends info to index.html and notes.html
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../publich/index.html'));
+});
+
+router.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/notes.html'));
+});
+
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
